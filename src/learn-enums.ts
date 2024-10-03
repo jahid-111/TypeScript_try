01a93d5c96d6398ebc4e579998b61d423078aabd
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-10-03 14:25:33 +0300

    enumsType

---
+++ src/learn-enums.ts
@@ -0,0 +1,14 @@
+
+//NUMBER
+enum Status {
+    NOT_STARTED, //= 0
+    IN_PROGRESS, //= 1
+    COMPLETED, //= 2
+}
+
+//STRING
+enum StatuS1 {
+    NOT_STARTED = "Not Started",
+    IN_PROGRESs = "In progress",
+    COMPLETED = "Completed"
+}   
