5821100fd00dd243b333fb904acce69103a9bc86
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-10-03 13:31:34 +0300

    array_useIn_Ts

---
+++ src/learn-array.ts
@@ -0,0 +1,10 @@
+let names: string[] = ["jahid", "developer"];
+
+let ages: Array<number> = [1, 2, 3, 4];
+
+let positions: (string | number)[] = [1, "One", "Two", 2];
+
+let multiDimArr: number[][] = [
+    [1, 2, 3, 4],
+    [5, 6, 7],
+];  
