4b09913b5ad9f47273b144115e69e7ebcd2d9d78
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-10-04 09:11:40 +0300

    typeToUse-ts

--- src/types.ts
+++ src/types.ts
@@ -30,3 +30,38 @@ const sales: Department = {
     name: "Sales",
     employee: [userDetails]
 }
+
+
+type ISBN = number | string;
+
+const book_a_isbn: ISBN = 1234;
+
+const book_b_isbn: ISBN = "1234"
+
+
+type Book = {
+    title: string,
+    page: number,
+    isbn: ISBN
+}
+
+type Writer = {
+    name: string,
+    age: number,
+    address: string
+}
+
+type BookWriter = Book & Writer;
+
+
+const bookAndWriter: BookWriter = {
+    title: "Deep Js",
+    page: 203,
+    isbn: 12323,
+    name: "Jahid",
+    age: 25,
+    address: "Bangladesh"
+}
+
+type startsWith = `Jahid-${string}`
+
